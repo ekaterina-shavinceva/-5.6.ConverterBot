f9f092515084bd3a1faaf96df5f4bbf4d61db43a
ekaterina-shavinceva <shavinceva-e@mail.ru>
2024-07-05 18:02:02 +0500

    Update extensions.py
    Считаем сумму количества валют return float(total_base * amount)

--- Project_Bot/extensions.py
+++ Project_Bot/extensions.py
@@ -29,4 +29,4 @@ class CurrencyConverter():
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={keys[quote]}&tsyms={keys[base]}')
         total_base = json.loads(r.content)[keys[base]]
 
-        return total_base
+        return float(total_base * amount)
